7fc917345b897f7b9452d3d17ed5f5d2927ee00c
tsubokura-shuhei <repeat2639@gmail.com>
2023-02-18 16:34:02 +0900

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import Layout from "../../components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+        <br />
+        <div className={utilStyles.lightText}>{postData.date}</div>
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.blogContentHTML }} />
+        <br />
+      </article>
+    </Layout>
+  );
+}
